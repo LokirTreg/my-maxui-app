ae4fd82b2e74db6b4ed37870ebf977a7aee85277
alexey.popovich <alexey.popovich@ANT-TSM4>
2026-04-13 14:52:09 +0300

    fix test

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,43 @@ import { useMaxWebApp } from './hooks/1';
 import { Arrival } from './pages/Arrival';
 
 function App() {
-  const { webApp, user } = useMaxWebApp();
-  var t = ""
+    const { webApp, user } = useMaxWebApp();
+    let t = ""
     if (webApp){
-        t = "WebApp есть"
+        t += "WebApp есть"
     }
-  if (1 == 1) {
-    return <Panel mode="secondary" className="panel">
-        <Arrival warehouseName="ворота" />;
-        <Panel id="panel">t</Panel>
-        </Panel>
-  }
+    if (user){
+        t += " user есть"
+    }
+    if (1 == 1) {
+        return <Panel className="panel">
+            <Arrival warehouseName="ворота" />
+            <Panel mode="secondary" id="panel">{t}</Panel>
+            </Panel>
+    }
 
-  return (
-      <Panel mode="secondary" className="panel">
-          <Grid gap={12} cols={1}>
-              <Container className="me">
-                  <Flex direction="column" align="center">
-                      <Avatar.Container size={72} form="squircle" className="me__avatar">
-                          <Avatar.Image src="https://sun9-21.userapi.com/1N-rJz6-7hoTDW7MhpWe19e_R_TdGV6Wu5ZC0A/67o6-apnAks.jpg" />
-                      </Avatar.Container>
+    return (
+        <Panel mode="secondary" className="panel">
+            <Grid gap={12} cols={1}>
+                <Container className="me">
+                    <Flex direction="column" align="center">
+                        <Avatar.Container size={72} form="squircle" className="me__avatar">
+                            <Avatar.Image src="https://sun9-21.userapi.com/1N-rJz6-7hoTDW7MhpWe19e_R_TdGV6Wu5ZC0A/67o6-apnAks.jpg" />
+                        </Avatar.Container>
 
-                      <Typography.Title>Иван Иванов</Typography.Title>
-                  </Flex>
-              </Container>
-              <Container className="me">
-                  <Button appearance="themed" mode="primary" onClick={() => {}} size="medium">
-                    Button
-                  </Button>
-                  <Button appearance="themed" mode="primary" onClick={() => {}} size="medium">
-                    Button
-                  </Button>
-              </Container>
-          </Grid>
-      </Panel>)
+                        <Typography.Title>Иван Иванов</Typography.Title>
+                    </Flex>
+                </Container>
+                <Container className="me">
+                    <Button appearance="themed" mode="primary" onClick={() => {}} size="medium">
+                        Button
+                    </Button>
+                    <Button appearance="themed" mode="primary" onClick={() => {}} size="medium">
+                        Button
+                    </Button>
+                </Container>
+            </Grid>
+        </Panel>)
 }
 
 export default App;
